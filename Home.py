a098a92c4467d259159bd10c0533fde9880ee9f9
starcoretcg <mike@starcoretcg.com>
2025-10-28 18:15:39 +0000

    Add Home page and fix Railway config

---
+++ Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="StarCore Card Generator", page_icon="🎴")
+
+st.title("🎴 StarCore TCG - Card Generator")
+st.markdown("### Welcome to the StarCore Card Generation System")
+st.info("👈 Select a generator from the sidebar")
